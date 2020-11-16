085f6f3cda5d8c7c7c81453d4d3ee0c3670496c6
dedoblege <dedoblege@gmail.com>
2020-11-16 17:01:31 +0100

    task 1: Output your main course goal with help of the composition API

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3>{{ courseGoal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button>Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -13,9 +13,15 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
+
 export default {
-  
-}
+  setup() {
+    const myGoal = reactive({ goal: 'Become a Vue Master' });
+
+    return { courseGoal: myGoal.goal };
+  }
+};
 </script>
 
 <style>
